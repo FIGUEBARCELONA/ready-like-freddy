072ad3763eecfa6c4ebbd1e1bc2527d06a7c0997
FIGUEBARCELONA <jordifiguerassolerpersonal@gmail.com>
2026-08-23 20:37:00 +0200

    RLF v25.54 register Delta 0049 suppliers and exclusions

--- apps/rlf-parallel50-v25-52/lib/known-suppliers.ts
+++ apps/rlf-parallel50-v25-52/lib/known-suppliers.ts
@@ -1,8 +1,10 @@
 export const KNOWN_SUPPLIER_DOMAINS=new Set([
   'remixshop.com','thrifttale.com','chakmarluxe.be','thrifted.mt','lemonsvintage.com','northerngrip.com','mojetvoje-secondhand.si','think2.eu','gems-vintage.de','peeces.de','re-nato.it','paavlikaltsukas.ee','onceagain.fr','jaama.de','vintageecoes.com','paulsboutiqueberlin.de','stillthrifting.de','rare-rags.de','alabamavintageshop.com','vintage-rags.de','nabevintage.com','2ndfit.de','rude-times.de','percentil.fr','modz.fr','preownedcasuals.de','thesecondlife.fr','vintageshit.de','lennysvintage.de','86casual.de','momoxfashion.com','37casual.de','stuffle.com','bettertimesclothing.de','retroarea.de','prelovedbaby.it','kurvengaenger.com','casualvintage.com','forgotten-fabrics.com','tntvintageclothing.com','back2grandvintage.com','emmeintheworld.it','claquettesmarket.com','kilo-shop.com',
-  'archetype.pl','barriquevintageshop.com','clothes4you.pl','clothest.it','culturavintage.es','dedstrangevintage.com','goofysanta.de','gtuned-vintage.de','impalavintage.com','kindakinks.es','ledestinsportif.com','lifelongtrends.com','lisbeths-erben.de','lote751vintage.com','mercatodelleoccasioni.it','micolet.com','picknweight.de','popbag.it','pretachanger.fr','vintagehere.com','thrift.cz','secondshot.cz','retrospectclothes.com','store.emmy.fi','vintagesportsclothing.com','vidanovapdl.com','fangovintage.com','tobiastore.com','vecchiostilevintageshop.com','rostreetwear.com','olesstore.com','second-soul.de','secondhandrosi.de'
+  'archetype.pl','barriquevintageshop.com','clothes4you.pl','clothest.it','culturavintage.es','dedstrangevintage.com','goofysanta.de','gtuned-vintage.de','impalavintage.com','kindakinks.es','ledestinsportif.com','lifelongtrends.com','lisbeths-erben.de','lote751vintage.com','mercatodelleoccasioni.it','micolet.com','picknweight.de','popbag.it','pretachanger.fr','vintagehere.com','thrift.cz','secondshot.cz','retrospectclothes.com','store.emmy.fi','vintagesportsclothing.com','vidanovapdl.com','fangovintage.com','tobiastore.com','vecchiostilevintageshop.com','rostreetwear.com','olesstore.com','second-soul.de','secondhandrosi.de',
+  'doubledoublevintage.com','buddyandselly.com','loopwestcoast.se'
 ]);
 
 export const KNOWN_REJECTED_DOMAINS=new Set([
-  'kingpinclothingstore.com','2dehands.be','mainlinemenswear.com','headlock.co','roundrobinclassics.com'
+  'kingpinclothingstore.com','2dehands.be','mainlinemenswear.com','headlock.co','roundrobinclassics.com',
+  'wikipedia.org','yelp.com','goldenpages.ie','thelabelfinder.com','worthpoint.com','gem.app','adverts.ie'
 ]);
