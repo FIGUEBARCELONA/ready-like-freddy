53961a6b5b54b0b19a21201d4eab9b78e13b1144
FIGUEBARCELONA <jordifiguerassolerpersonal@gmail.com>
2026-08-24 18:51:46 +0200

    Add canonical supplier aliases and reject non-supplier directories

--- apps/rlf-parallel50-v25-52/lib/known-suppliers.ts
+++ apps/rlf-parallel50-v25-52/lib/known-suppliers.ts
@@ -5,6 +5,10 @@ export const KNOWN_SUPPLIER_DOMAINS=new Set([
   '2ndfit.de','37casual.de','86casual.de','alabamavintageshop.com','archetype.pl','asti.mercatopoli.it','back2grandvintage.com','barriquevintageshop.com','bettertimesclothing.de','buddyandselly.com','casualvintage.com','chakmarluxe.be','claquettesmarket.com','clothes4you.pl','clothest.it','culturavintage.es','dedstrangevintage.com','dimanoinmano.it','doubledoublevintage.com','emmeintheworld.it','fangovintage.com','forgotten-fabrics.com','gems-vintage.de','goofysanta.de','gtuned-vintage.de','impalavintage.com','jaama.de','kilo-shop.com','kindakinks.es','kingkongathensvintage.gr','kurvengaenger.com','ledestinsportif.com','lemonsvintage.com','lennysvintage.de','lifelongtrends.com','lisbeths-erben.de','loopwestcoast.se','lote751vintage.com','mercatodelleoccasioni.it','micolet.com','micolet.pt','modz.fr','mojetvoje-secondhand.si','momoxfashion.com','nabevintage.com','northerngrip.com','olesstore.com','onceagain.fr','orzinuovi.mercatopoli.it','paavlikaltsukas.ee','paulsboutiqueberlin.de','peeces.de','percentil.fr','picknweight.de','popbag.it','prelovedbaby.it','preownedcasuals.de','pretachanger.fr','rare-rags.de','re-nato.it','remixshop.com','retroarea.de','retrospectclothes.com','rostreetwear.com','rostreewear.com','rostretwear.com','rude-times.de','santospiritovintage.com','saronno.mercatopoli.it','second-soul.de','secondamanina.it','secondhandrosi.de','secondshot.cz','sellpy.com','sonnechko.com','stillthrifting.de','store.emmy.fi','stuffle.com','thesecondlife.fr','think2.eu','thrift.cz','thrifted.mt','thrifttale.com','tilt-vintage.com','tntvintageclothing.com','tobiastore.com','vecchiostilevintageshop.com','vidanovapdl.com','vinokilo.events','vintage-rags.de','vintageecoes.com','vintagehere.com','vintager2.de','vintageshit.de','vintagesportsclothing.com'
 ]);
 
+export const KNOWN_SUPPLIER_ALIAS_DOMAINS=new Set([
+  'sellpy.fr','sellpy.se','sellpy.de','sellpy.nl','sellpy.fi','sellpy.dk','sellpy.at','sellpy.be'
+]);
+
 export const STAGED_SUPPLIER_DOMAINS=new Set([
   'vintagehere.com','thrift.cz','vintagesportsclothing.com','vidanovapdl.com','fangovintage.com','tobiastore.com','vecchiostilevintageshop.com','rostreetwear.com','olesstore.com','doubledoublevintage.com','buddyandselly.com','loopwestcoast.se'
 ]);
@@ -14,7 +18,7 @@ export const KNOWN_IDENTITY_KEYS=new Set([
 ]);
 
 export const KNOWN_REJECTED_DOMAINS=new Set([
-  '2dehands.be','adverts.ie','bazar.bg','cybo.com','fashiola.com','fashiola.de','fashiola.fr','gem.app','goldenpages.ie','headlock.co','joinfleek.com','kingpinclothingstore.com','kurpirkt.lv','loopi.com','mainlinemenswear.com','mapcarta.com','near-place.com','novacircle.com','one4all.mt','pagesmode.com','roundrobinclassics.com','stockholmfashiondistrict.se','taobao.com','thelabelfinder.com','twintipuk.myshopify.com','vendora.bg','vintageclothingguides.com','wikipedia.org','world.taobao.com','worthpoint.com','yellow.com.mt','yelp.com'
+  '2dehands.be','adverts.ie','annuaire-entreprises.data.gouv.fr','bazar.bg','cybo.com','entreprises.lefigaro.fr','fashiola.com','fashiola.de','fashiola.fr','gem.app','goldenpages.ie','headlock.co','joinfleek.com','kingpinclothingstore.com','kuantokusta.pt','kurpirkt.lv','loopi.com','mainlinemenswear.com','mapcarta.com','near-place.com','novacircle.com','one4all.mt','pagesjaunes.fr','pagesmode.com','pappers.fr','roundrobinclassics.com','stockholmfashiondistrict.se','taobao.com','thelabelfinder.com','twintipuk.myshopify.com','vendora.bg','vintageclothingguides.com','wikipedia.org','world.taobao.com','worthpoint.com','yellow.com.mt','yelp.com'
 ]);
 
 export const KNOWN_IDENTITY_QUARANTINE_DOMAINS=new Set([
@@ -24,6 +28,7 @@ export const KNOWN_IDENTITY_QUARANTINE_DOMAINS=new Set([
 export const CANONICAL_REGISTRY_COVERAGE={
   expectedIdentities:CANONICAL_REGISTRY_EXPECTED_IDENTITIES,
   materializedDomains:KNOWN_SUPPLIER_DOMAINS.size,
+  materializedAliasDomains:KNOWN_SUPPLIER_ALIAS_DOMAINS.size,
   materializedIdentityKeys:KNOWN_IDENTITY_KEYS.size,
   identityQuarantineDomains:KNOWN_IDENTITY_QUARANTINE_DOMAINS.size,
   complete:false,
